32a0f201a52597391280d0e216702d4070956c7a
Mohit Pandey <mohitpaandey009@gmail.com>
2023-02-14 23:55:14 +0530

    initial

---
+++ src/Check/TargetPortfolio.java
@@ -0,0 +1,28 @@
+package Check;
+
+import java.math.BigDecimal;
+
+public class TargetPortfolio {
+public BigDecimal taaDuration;
+public BigDecimal calculatedWeight;
+public BigDecimal weight;
+public BigDecimal getTaaDuration() {
+	return taaDuration;
+}
+public void setTaaDuration(BigDecimal taaDuration) {
+	this.taaDuration = taaDuration;
+}
+public BigDecimal getCalculatedWeight() {
+	return calculatedWeight;
+}
+public void setCalculatedWeight(BigDecimal calculatedWeight) {
+	this.calculatedWeight = calculatedWeight;
+}
+public BigDecimal getWeight() {
+	return weight;
+}
+public void setWeight(BigDecimal weight) {
+	this.weight = weight;
+}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author SDIEA
+ *
+ */
+module Demo {
+}
